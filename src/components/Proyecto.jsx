cf36268eb26a69b96b4d273bd90849296aa0adca
patricio355 <patricioquiroga35@gmail.com>
2025-04-24 14:58:41 -0300

    arreglo nav

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -3,13 +3,16 @@ import './Proyecto.css';
 
 const Proyecto = ({ titulo, descripcion, link }) => {
   return (
-    <div className="card">
+    
+      <div className="card">
       <h3>{titulo}</h3>
       <p>{descripcion}</p>
       <a href={link} target="_blank" rel="noopener noreferrer" className="btn-link">
         Ver Proyecto
       </a>
     </div>
+   
+    
   );
 };
 
